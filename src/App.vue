1cd5c87d0d8500b7277fa95a93b57f3355a34016
Math3usH <matheushenriquecalsavara@gmail.com>
2024-02-23 16:31:44 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,32 @@ import { ref } from 'vue'
 
 const contador = ref(0)
 
-function cecrementar() {
+function decrementar() {
   contador.value--
 }
-
+function zerar(){
+  contador.value = 0
+}
 </script>
 
 <template>
   <button @click="contador++">+</button>
   <button @click="decrementar">-</button>
+  <button @click="zerar">C</button>
   <p>o valor do contador é: {{ contador }}</p>
 </template>
 
 <style scoped>
+button {
+  font-size: 2rem;
+  padding: 1rem;
+  margin: 1rem;
+  cursor: pointer;
+  border-radius: 10px;
+  
+}
 
+button:hover{
+  background-color: rgb(99, 99, 99);
+}
 </style>
